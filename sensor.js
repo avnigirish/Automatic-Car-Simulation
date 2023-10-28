db4bb59ed2daa4d61d95cb44ce46a49fffec6fcf
Avni Girish <avni123.girish@gmail.com>
2023-10-28 00:37:10 -0400

    Simulating traffic

--- sensor.js
+++ sensor.js
@@ -9,18 +9,18 @@ class Sensor{
         this.readings=[];
     }
 
-    update(roadBorders){
+    update(roadBorders, traffic){
         this.#castRays();
         this.readings=[];
         for(let i=0;i<this.rays.length;i++){
             this.readings.push(
-                this.#getReading(this.rays[i],roadBorders)
+                this.#getReading(this.rays[i],roadBorders, traffic)
             );
         }
     }
 
     //get detection of the road borders, gets closest one for the reading
-    #getReading(ray,roadBorders){
+    #getReading(ray,roadBorders, traffic){
         let touches=[];
 
         for(let i=0;i<roadBorders.length;i++){
@@ -33,6 +33,21 @@ class Sensor{
             );
             if(touch){
                 touches.push(touch);
+            }
+        }
+
+        for(let i=0; i<traffic.length; i++){
+            const poly=traffic[i].polygon;
+            for(let j=0; j<poly.length; j++){
+                const value=getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j+1)%poly.length]
+                );
+                if(value){
+                    touches.push(value);
+                }
             }
         }
 
